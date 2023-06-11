1748ea15c7289b1161ce1fb6c3551dec39d8d46b
Mamitiana <mamitiana@fulldigits.com>
2023-06-11 11:49:51 +0300

    29:03 basic usage of fn_map in front

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 const Home = () => {
-  return(
+  return (
     <>
-        HeroBanner
+      HeroBanner
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div>
+        {['Product 1', 'Product 2'].map((product) => product)}
+      </div>
     </>
-  ) 
-  
-  ;
+  );
 };
 
 export default Home;
