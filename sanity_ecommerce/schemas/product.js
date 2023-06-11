e4121c7519048235bdc2e4e0cc963ba8a568517e
Mamitiana <mamitiana@fulldigits.com>
2023-06-11 10:33:37 +0300

    18:13 Creating a product_schema in Sanity; hotspot means you can crop the image to show

---
+++ sanity_ecommerce/schemas/product.js
@@ -0,0 +1,20 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'array',
+      of: [
+        {
+          type: 'image',
+        },
+      ],
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+}
