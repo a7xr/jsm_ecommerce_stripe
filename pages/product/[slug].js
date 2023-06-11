5924c7f482df2ef1fa2ad342414ce00d8ab78e24
Mamitiana <mamitiana@fulldigits.com>
2023-06-11 18:50:04 +0300

    1:27:57 using getStaticProps needs getStaticPaths... getStaticPaths is going to grab the clicked product and we are going to manage that image that we clicked

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -16,6 +16,20 @@ const ProductDetails = ({ products, product }) => {
   );
 };
 
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`;
+  const products = await client.fetch(query);
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+};
+
 export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
   const productsQuery = '*[_type == "product"]';
