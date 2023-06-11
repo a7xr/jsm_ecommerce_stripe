6b4436d9c807249bb8c307f97018d4e90bf99744
Mamitiana <mamitiana@fulldigits.com>
2023-06-11 17:20:21 +0300

    1:10:19 compLayout is now grabbing all components

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
     <div className='layout'>
         <Head>
@@ -12,11 +13,14 @@ const Layout = () => {
             </title>
         </Head>
         <header>
-            <Navbar></Navbar>
+            <Navbar />
         </header>
         <main className='main-container'>
-
+            {children}
         </main>
+        <footer>
+            <Footer />
+        </footer>
     </div>
   )
 }
