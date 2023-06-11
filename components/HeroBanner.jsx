26274cd401c4ea45d5817e31c526bb96cb229719
Mamitiana <mamitiana@fulldigits.com>
2023-06-11 15:01:02 +0300

    52:59 in the front, customizing the button of the product + the description

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -17,12 +17,12 @@ const HeroBanner = ({heroBanner}) => {
             </h1>
             <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
             <div>
-                <Link href='/product'>
-                    <button type="button">BUTTON TEXT</button>
+                <Link href={`/product/${heroBanner.product}`}>
+                    <button type="button">{heroBanner.buttonText}</button>
                 </Link>
                 <div className="desc">
                     <h5>Description</h5>
-                    <p>DESCRIPTION</p>
+                    <p>{heroBanner.desc}</p>
                 </div>
             </div>
         </div>
