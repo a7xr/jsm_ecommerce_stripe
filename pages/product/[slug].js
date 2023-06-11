d15e0f8e4627e0454fbd27aaefdd92ed5863ed1f
Mamitiana <mamitiana@fulldigits.com>
2023-06-11 20:03:39 +0300

    1:37:51 Adding the banner "You may also like"

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -6,6 +6,7 @@ import {
   AiFillStar,
   AiOutlineStar,
 } from "react-icons/ai";
+import { Product } from "../../components";
 
 const ProductDetails = ({ products, product }) => {
   const { image, name, details, price } = product;
@@ -64,6 +65,16 @@ const ProductDetails = ({ products, product }) => {
             <button type="button" className="buy-now" onClick="">
               Buy Now
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>
