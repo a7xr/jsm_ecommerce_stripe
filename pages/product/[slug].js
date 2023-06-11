d1315faba8ef0580715414b1f466df8afca4f1e4
Mamitiana <mamitiana@fulldigits.com>
2023-06-11 19:49:29 +0300

    1:33:17 Adding the button to choose the quantity of /product/XXX

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -43,6 +43,20 @@ const ProductDetails = ({ products, product }) => {
           <h4>Details: </h4>
           <p>{details}</p>
           <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity: </h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />
+              </span>
+              <span className="num" onClick="">
+                0
+              </span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
