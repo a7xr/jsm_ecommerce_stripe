99bb8ee91fc8b95a1140a6285b0ec0789455040f
Mamitiana <mamitiana@fulldigits.com>
2023-06-11 21:12:09 +0300

    1:47:09 The states that we needs to manage when buying with Stripe:     showCart is false at the start, will show the cart or not     cartItems, what is the item that we have in our cart     totalPrice     totalQuantities, what is the total of the qty that we are working with     qty

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,1 +1,25 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const [qty, setQty] = useState(1);
+
+  return (
+    <Context.Provider
+      value={{
+        showCart,
+        cartItems,
+        totalPrice,
+        totalQuatities,
+        qty
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
