bf45944e6c5aeee170064868865dc237a66e2490
Mamitiana <mamitiana@fulldigits.com>
2023-06-11 11:52:09 +0300

    Minor fix

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,8 @@ const Home = () => {
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>
-        {['Product 1', 'Product 2'].map((product) => product)}
-      </div>
+      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      Footer
     </>
   );
 };
