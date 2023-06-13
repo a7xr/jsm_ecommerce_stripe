09e1dff8d10986b4ea20da76f30ef458a93fcbd8
Mamitiana <mamitiana@fulldigits.com>
2023-06-13 05:32:47 +0300

    1:58:34 if product not yet in the cart and we add the item

--- context/StateContext.js
+++ context/StateContext.js
@@ -28,8 +28,11 @@ export const StateContext = ({ children }) => {
           };
       });
       setCartItems(updatedCartItems);
-      toast.success(`${qty} ${product.name} added to the cart`)
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
     }
+    toast.success(`${qty} ${product.name} added to the cart`);
   };
 
   const incQty = () => {
