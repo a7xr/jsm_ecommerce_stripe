123494b13097fadd3ba5652047815547eb324615
Mamitiana <mamitiana@fulldigits.com>
2023-06-14 11:50:25 +0300

    2:26:34 Adding in the compCart the fn of inc and decr items

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -9,7 +9,7 @@ import { urlFor } from '../lib/client';
 
 const Cart = () => {
   const cartRef = useRef()
-  const {totalPrice, totalQuantities, cartItems, setShowCart} = useStateContext();
+  const {totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity} = useStateContext();
 
   return (
     <div className='cart-wrapper' ref={cartRef}>
@@ -58,11 +58,11 @@ const Cart = () => {
                   <div className="flex bottom">
                     <div>
                         <p className="quantity-desc">
-                            <span className="minus" onClick="">
+                            <span className="minus" onClick={() => toggleCartItemQuantity(item._id, 'desc')}>
                                 <AiOutlineMinus />
                             </span>
-                            <span className="num">0</span>
-                            <span className="plus" onClick="">
+                            <span className="num">{item.quantity}</span>
+                            <span className="plus" onClick={() => toggleCartItemQuantity(item._id, 'inc')}>
                                 <AiOutlinePlus />
                             </span>
                         </p>
